e460a4133b92bc40490905aed1d7216ff7f098b1
ibaris <i.baris@outlook.de>
2018-11-10 13:59:23 +0100

    Update init imports

--- radarpy/emw/__init__.py
+++ radarpy/emw/__init__.py
@@ -1,1 +1,2 @@
-from emw import *
+from .emw import (EMW, compute_wavelength, compute_frequency, compute_wavenumber, convert_frequency,
+                  convert_wavelength, select_band, select_region, which_band, which_region)
