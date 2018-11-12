207464e183c771fa01e4652952b8af6ca31725b7
ibaris <i.baris@outlook.de>
2018-11-12 19:29:31 +0100

    [respy.auxiliary] Add PI and C as global

--- respy/__init__.py
+++ respy/__init__.py
@@ -1,6 +1,6 @@
 from .angles import Angles
 from .auxiliary import (rad, deg, sec, cot, align_all, max_length, asarrays, same_len, stacks, zeros_likes, inf_to_num,
                         get_geometries)
-from .conversion import dB, linear, BSC, BRDF, BRF
+from .conversion import dB, linear, BSC, BRDF, BRF, Conversion
 from .emw import (EMW, compute_wavelength, compute_frequency, compute_wavenumber, convert_frequency,
                   convert_wavelength, select_band, select_region, which_band, which_region)
