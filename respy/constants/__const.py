9db6a1ee876f44316bd4945248bfc3a1c72cf4f4
ibaris <i.baris@outlook.de>
2019-01-16 16:53:38 +0100

    feat(respy.constants): Add constants

--- respy/constants/__const.py
+++ respy/constants/__const.py
@@ -5,3 +5,5 @@ k_B = Quantity(1.38064852e-23, name="Boltzmann constant", unit="joule / kelvin",
 c = Quantity(299792458.0, name="Speed of light in vacuum", unit="meter / second", constant=True)
 h = Quantity(6.62606957e-34, name="Planck constant", unit="joule * second", constant=True)
 pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164
+rad_to_deg = 180.0 / pi
+deg_to_rad = pi / 180.0
