c2ed2a1dfaebf90f0fc3bd727c6bb7b7bb61ec5b
ibaris <i.baris@outlook.de>
2019-01-16 16:53:16 +0100

    feat(respy.conversion.Conversion): Add static methods to Conversion

--- respy/conversion.py
+++ respy/conversion.py
@@ -2,7 +2,8 @@ from __future__ import division
 
 from numpy import cos, log10, errstate, nan_to_num
 
-from respy.auxiliary import rad, PI
+import respy.constants as const
+from respy.util import rad, __ANGLE_UNIT_DEG__, __ANGLE_UNIT_RAD__
 
 
 class Conversion(object):
@@ -61,136 +62,128 @@ class Conversion(object):
 
         if self.value_unit is "BRDF":
             self.BRDF = value
-            self.BSC = BSC(value, vza, self.angle_unit)
-            self.BSCdB = dB(BSC(value, vza, self.angle_unit))
-            self.BRF = BRF(value)
+            self.BSC = Conversion.BSC(value, vza, self.angle_unit)
+            self.BSCdB = Conversion.dB(Conversion.BSC(value, vza, self.angle_unit))
+            self.BRF = Conversion.BRF(value)
 
         elif self.value_unit is "BSC":
             self.BSC = value
-            self.BRDF = BRDF(value, vza, self.angle_unit)
-            self.BRF = BRF(self.BRDF)
-            self.BSCdB = dB(value)
+            self.BRDF = Conversion.BRDF(value, vza, self.angle_unit)
+            self.BRF = Conversion.BRF(self.BRDF)
+            self.BSCdB = Conversion.dB(value)
 
         elif self.value_unit is "BSCdB":
             self.BSCdB = value
-            self.BSC = linear(value)
-            self.BRDF = BRDF(self.BSC, vza, self.angle_unit)
-            self.BRF = BRF(self.BRDF)
+            self.BSC = Conversion.linear(value)
+            self.BRDF = Conversion.BRDF(self.BSC, vza, self.angle_unit)
+            self.BRF = Conversion.BRF(self.BRDF)
 
         elif self.value_unit is "BRF":
             self.BRF = value
-            self.BRDF = value / PI
-            self.BSC = BSC(self.BRDF, vza, self.angle_unit)
-            self.BSCdB = dB(BSC(self.BRDF, vza, self.angle_unit))
+            self.BRDF = value / const.pi
+            self.BSC = Conversion.BSC(self.BRDF, vza, self.angle_unit)
+            self.BSCdB = Conversion.dB(Conversion.BSC(self.BRDF, vza, self.angle_unit))
 
         else:
             raise ValueError("the unit of value must be 'BRDF', 'BRF', 'BSC' or 'BSCdB'")
 
-
     @staticmethod
     def dB(x):
-        return dB(x)
+        """
+        Convert a linear value to dB.
+        """
+        with errstate(invalid='ignore'):
+            return nan_to_num(10 * log10(x))
 
     @staticmethod
     def linear(x):
-        return linear(x)
+        """
+        Convert a dB value in linear.
+        """
+        return 10 ** (x / 10)
+
+    @staticmethod
+    def BRDF(BSC, vza, angle_unit='RAD'):
+        """
+        Convert a Radar Backscatter Coefficient (BSC) into a BRDF.
+
+        Note
+        -----
+        Hot spot direction is vza == iza and raa = 0.0
+
+        Parameters
+        ----------
+        BSC : int, float or array_like
+            Radar Backscatter Coefficient (sigma 0).
+        vza : int, float or array_like
+            View or scattering zenith angle.
+        angle_unit : {'DEG', 'RAD'} (default = 'RAD'), optional
+            * 'DEG': All input angles (iza, vza, raa) are in [DEG].
+            * 'RAD': All input angles (iza, vza, raa) are in [RAD].
+
+        Returns
+        -------
+        BRDF value : int, float or array_like
+
+        """
+        if angle_unit in __ANGLE_UNIT_RAD__:
+            return BSC / (cos(vza) * (4 * const.pi))
+
+        elif angle_unit in __ANGLE_UNIT_DEG__:
+            return BSC / (cos(rad(vza)) * (4 * const.pi))
+        else:
+            raise ValueError("angle_unit must be 'RAD' or 'DEG'")
+
+    @staticmethod
+    def BRF(BRDF):
+        """
+        Convert a BRDF into a BRF.
+
+        Note
+        -----
+        Hot spot direction is vza == iza and raa = 0.0
+
+        Parameters
+        ----------
+        BRDF : int, float or array_like
+            BRDF value.
+
+        Returns
+        -------
+        BRF value : int, float or array_like
+
+        """
+        return const.pi * BRDF
+
+    @staticmethod
+    def BSC(BRDF, vza, angle_unit='RAD'):
+        """
+        Convert a BRDF in to a Radar Backscatter Coefficient (BSC).
+
+        Note
+        -----
+        Hot spot direction is vza == iza and raa = 0.0
+
+        Parameters
+        ----------
+        BSC : int, float or array_like
+            Radar Backscatter Coefficient (sigma 0).
+        vza : int, float or array_like
+            View or scattering zenith angle.
+        angle_unit : {'DEG', 'RAD'} (default = 'RAD'), optional
+            * 'DEG': All input angles (iza, vza, raa) are in [DEG].
+            * 'RAD': All input angles (iza, vza, raa) are in [RAD].
+
+        Returns
+        -------
+        BRDF value : int, float or array_like
+
+        """
+        if angle_unit in __ANGLE_UNIT_RAD__:
+            return BRDF * cos(vza) * 4 * const.pi
 
 
-def dB(x):
-    """
-    Convert a linear value to dB.
-    """
-    with errstate(invalid='ignore'):
-        return nan_to_num(10 * log10(x))
-
-
-def linear(x):
-    """
-    Convert a dB value in linear.
-    """
-    return 10 ** (x / 10)
-
-
-def BRDF(BSC, vza, angle_unit='RAD'):
-    """
-    Convert a Radar Backscatter Coefficient (BSC) into a BRDF.
-
-    Note
-    -----
-    Hot spot direction is vza == iza and raa = 0.0
-
-    Parameters
-    ----------
-    BSC : int, float or array_like
-        Radar Backscatter Coefficient (sigma 0).
-    vza : int, float or array_like
-        View or scattering zenith angle.
-    angle_unit : {'DEG', 'RAD'} (default = 'RAD'), optional
-        * 'DEG': All input angles (iza, vza, raa) are in [DEG].
-        * 'RAD': All input angles (iza, vza, raa) are in [RAD].
-
-    Returns
-    -------
-    BRDF value : int, float or array_like
-
-    """
-    if angle_unit == 'RAD':
-        return BSC / (cos(vza) * (4 * PI))
-
-    elif angle_unit == 'DEG':
-        return BSC / (cos(rad(vza)) * (4 * PI))
-    else:
-        raise ValueError("angle_unit must be 'RAD' or 'DEG'")
-
-
-def BRF(BRDF):
-    """
-    Convert a BRDF into a BRF.
-
-    Note
-    -----
-    Hot spot direction is vza == iza and raa = 0.0
-
-    Parameters
-    ----------
-    BRDF : int, float or array_like
-        BRDF value.
-
-    Returns
-    -------
-    BRF value : int, float or array_like
-
-    """
-    return PI * BRDF
-
-
-def BSC(BRDF, vza, angle_unit='RAD'):
-    """
-    Convert a BRDF in to a Radar Backscatter Coefficient (BSC).
-
-    Note
-    -----
-    Hot spot direction is vza == iza and raa = 0.0
-
-    Parameters
-    ----------
-    BSC : int, float or array_like
-        Radar Backscatter Coefficient (sigma 0).
-    vza : int, float or array_like
-        View or scattering zenith angle.
-    angle_unit : {'DEG', 'RAD'} (default = 'RAD'), optional
-        * 'DEG': All input angles (iza, vza, raa) are in [DEG].
-        * 'RAD': All input angles (iza, vza, raa) are in [RAD].
-
-    Returns
-    -------
-    BRDF value : int, float or array_like
-
-    """
-    if angle_unit == 'RAD':
-        return BRDF * cos(vza) * 4 * PI
-
-    elif angle_unit == 'DEG':
-        return BRDF * cos(rad(vza)) * (4 * PI)
-    else:
-        raise ValueError("angle_unit must be 'RAD' or 'DEG'")
+        elif angle_unit in __ANGLE_UNIT_DEG__:
+            return BRDF * cos(rad(vza)) * (4 * const.pi)
+        else:
+            raise ValueError("angle_unit must be 'RAD' or 'DEG'")
