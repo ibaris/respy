44511255ce5e6d8a96cb795dcbe21eef42119717
ibaris <i.baris@outlook.de>
2019-02-04 19:16:42 +0100

    chore(respy.table): Add table framework

--- table.py
+++ table.py
@@ -5,14 +5,16 @@ Spyder Editor
 This is a temporary script file.
 """
 
-def test(*args):
-    print args
-    #for item in args.keys():
-    #    print item
-        
-t = {'a':3, 'b':5}
+# def test(*args):
+#     print args
+#     #for item in args.keys():
+#     #    print item
+#
+# t = {'a':3, 'b':5}
 
 import pandas as pd
+import numpy as np
+from respy.units import Quantity
 
 class SubclassedSeries(pd.Series):
 
@@ -24,266 +26,149 @@ class SubclassedSeries(pd.Series):
     def _constructor_expanddim(self):
         return SubclassedDataFrame
 
-    def __repr__(self):
-        print "yep!"
-        
+
 class SubclassedDataFrame(pd.DataFrame):
+    _attributes_ = ["header"]
+
+    def __init_(self, data=None, index=None, columns=None, dtype=None, copy=False, units=None, description=None):
+        super(SubclassedDataFrame, self).__init_(data=data, index=index, columns=columns, dtype=dtype, copy=copy)
+
+        self.__set_table(units, description)
+
+        self._attributes_[0]._copy_attrs(self)
+
+    def _copy_attrs(self, df):
+        for attr in self._attributes_.split(","):
+            df.__dict__[attr] = getattr(self, attr, None)
+
+    def __set_table(self, units, description):
+        index_name = '*' + self.index.name if self.index.name != None else '*Index'
+        index_dtype = self.index.dtype
+        index_value = self.index.values
+
+        columns = self.columns
+        values = self.values
+
+
+        # values = [self[item].values for item in columns]
+
+        dtypes_str = [str(self[item].dtypes) for item in columns]
+        dtypes = [self[item].dtypes for item in columns]
+
+        total_values = np.zeros((values.shape[0], values.shape[1] + 1))
+        total_dtypes = list()
+        total_columns = list()
+
+        total_columns.append(index_name)
+        total_dtypes.append(str(index_dtype))
+
+        total_values[:, 0] = index_value
+
+        self.columns = list()
+        self.dtype = list()
+        self.__dtype_str = list()
+
+        for i, _ in enumerate(columns):
+            self.columns.append(columns[i])
+            self.dtype.append(dtypes[i])
+            self.__dtype_str.append(dtypes_str[i])
+
+        unit_list = list()
+        if units is None:
+            unit_list = ['[-]' for i in self.columns]
+
+        elif len(units) != len(self.columns):
+            raise ValueError
+        else:
+            pass
+
+        self.all_values = total_values
+        self.__values_str = total_values.astype(np.str)
+
+        len_str_array = np.zeros(self.all_values.shape[1], dtype=np.int)
+
+        for i in range(self.all_values.shape[1]):
+            sliced = self.__values_str[:, i]
+            len_list = list()
+
+            for j in range(sliced.shape[0]):
+                len_list.append(len(sliced[j]))
+
+            len_str_array[i] = max(len_list)
+
+        len_columns = [len(item) for item in total_columns]
+        len_dtypes = [len(item) for item in total_dtypes]
+
+        zipped_lens = zip(len_columns, len_dtypes, len_str_array)
+
+        self.__max_lens = [max(item) for item in zipped_lens]
+        self.__stripes = [self.make_stripe(max(item)) for item in zipped_lens]
+
+        body = list()
+        columns_line = ''
+        dtype_line = ''
+        stripes_line = ''
+
+        # total_values_T = [item.transpose() for item in total_values]
+
+        self.__header = ''
+        for i in range(len(total_columns)):
+            columns_line += total_columns[i].rjust(self.__max_lens[i]) + '  '
+            dtype_line += total_dtypes[i].rjust(self.__max_lens[i]) + '  '
+            stripes_line += self.__stripes[i].rjust(self.__max_lens[i]) + '  '
+
+            self.__header = columns_line + '\n' + dtype_line + '\n' + stripes_line + '\n'
+
+
+
 
     @property
     def _constructor(self):
-        return SubclassedDataFrame
+        def f(*args, **kw):
+            df = SubclassedDataFrame(*args, **kw)
+            self._copy_attrs(df)
+            return df
+        return f
 
     @property
     def _constructor_sliced(self):
         return SubclassedSeries
-    
-    def make_text_for_repr(self):
-        text = str(self).split('\n')
-        header = text[0]
-        body = [text[1:][i].split() for i in range(len(text[1:]))]
-        header_split = header.split()
-        dtypes = [self.dtypes[item] for item in header_split]
-        stripes = [self.make_stripe(len(str(item))) for item in dtypes]
 
-
-        len_header = [len(item) for item in header_split]
-        len_stripes = [len(item) for item in stripes]
-                       
-        stripes_align = list()
-        header_align = list()
-        dtype_align = list()
-        body_align = list() 
-        
-        for i in range(len(len_header)):
-            lheader = len_header[i]
-            lstripes = len_stripes[i]
-            
-            if lheader > lstripes:
-                 header_align.append(header_split[i])
-                 stripes_align.append(stripes[i].rjust(lheader))
-                 dtype_align.append(str(dtypes[i]).rjust(lheader))
-            else:
-                 header_align.append(header_split[i].rjust(lstripes))
-                 stripes_align.append(stripes[i])
-                 dtype_align.append(str(dtypes[i]))
-
-        for i in range(len(body)):
-            lheader = len_header[i]
-            lstripes = len_stripes[i]
-
-            if lheader > lstripes:
-                 body_align.append([body[i][j].rjust(lheader) for j in range(len(body[i]))])
-            else:
-                 body_align.append([body[i][j].rjust(lstripes) for j in range(len(body[i]))])
-            
-            body_align[i][0] = body_align[i][0].split()[0]
-
-        body_text = list()
-        
-        
-        for i in range(len(body_align)):
-            body_temp = ''
-            for j in range(len(body_align[i])):
-                if j == len(body_align[i])-1:
-                    body_temp += body_align[i][j]
-                else:
-                    body_temp += body_align[i][j] + '  '
-            
-            body_text.append(body_temp)
-        
-        space = ''
-        for item in header:
-            if item is ' ':
-                space += item
-            else:
-                break
-
-        dtype_temp = ''
-        stripes_temp = ''
-        header_temp = ''
-        for i in range(len(header_split)):
-            if i == len(header_split)-1:
-                dtype_temp += dtype_align[i]
-                stripes_temp += stripes_align[i]
-                header_temp += header_align[i]
-            else:
-                dtype_temp += dtype_align[i]  + '  '
-                stripes_temp += stripes_align[i] + '  '
-                header_temp += header_align[i] + '  '
-        
-        items = [header_temp, dtype_temp, stripes_temp]
-            
-        
-#        for i in range(len(body_text)):
-#            body_text[i] = space + body_text[i]
-
-        for i in range(len(items)):
-            items[i] = space + items[i]
-
-        text_header = ''
-        for i in range(len(items)):
-            text_header += items[i] + '\n'
-            
-        text_body = ''
-        for i in range(len(body_text)):
-            text_body += body_text[i] + '\n'
-            
-        total_text = text_header + text_body
-        #print total_text
-        
-        return total_text
-    
     def space(self, n):
         space = ''
         for i in range(n):
             space += ' '
         return space
-            
+
     def __repr__(self):
-        text = str(self).split('\n')
-        header  = text[0]
-        if self.index.name is None:
-            body_all = [text[1:][i].split() for i in range(len(text[1:]))]
-            index_name = None
-        else:
-            index_name = text[1].split()
-            body_all = [text[2:][i].split() for i in range(len(text[2:]))]
-        
-        index = list()
-        
-        body = list()
-        for i in range(len(body_all)):
-            body_atom = list()
-            for j in range(len(body_all[i])):
-                if j == 0:
-                    index.append(body_all[i][j])
-                else:
-                    body_atom.append(body_all[i][j])
-            body.append(body_atom)
-            
-        header_split = header.split()
-        dtypes = [self.dtypes[item] for item in header_split]
-        stripes = [self.make_stripe(len(str(item))) for item in dtypes]
 
+        for i in range(self.all_values.shape[0]):
+            value_row = self.__values_str[i]
 
-        len_header = [len(item) for item in header_split]
-        len_stripes = [len(item) for item in stripes]
-        len_index_name = len(index_name[0])
-        len_index = [len(item) for item in index]
-                       
-        stripes_align = list()
-        header_align = list()
-        dtype_align = list()
-        body_align = list() 
-    
-        for i in range(len(len_header)):
-            lheader = len_header[i]
-            lstripes = len_stripes[i]
-            lindexname = len_index_name
-            lindex = len_index[i]
-            
-            prefix = [lindexname, lindex]
-            spaces = [lheader, lstripes, lindexname, lindex]
-            max_len = max(spaces)
-            max_prefix = max(prefix)
-            
-            if i ==  0:
-                sp = self.space(max_prefix + 2)
-            else:
-                sp = ''
-                
-            header_align.append(sp + header_split[i].rjust(max_len))
-            stripes_align.append(sp + stripes[i].rjust(max_len))
-            dtype_align.append(sp + str(dtypes[i]).rjust(max_len))
-            
+            row = ''
 
-        for i in range(len(body)):
-            lheader = len_header[i]
-            lstripes = len_stripes[i]
-            lindexname = len_index_name
-            lindex = len_index[i]
-            
-            prefix = [lindexname, lindex]
-            spaces = [lheader, lstripes, lindexname, lindex]
-            max_len = max(spaces)
-            max_prefix = max(prefix)
-            sp = self.space(max_prefix + 2)
-            
-            body[i][0] = sp + body[i][0]
-            
-                
-            body_align.append([body[i][j].rjust(max_len) for j in range(len(body[i]))])
-            
+            for j in range(value_row.shape[0]):
+                row += value_row[j].rjust(self.__max_lens[j]) + '  '
 
-        body_text = list()
-        
-        
-        for i in range(len(body_align)):
-            body_temp = ''
-            for j in range(len(body_align[i])):
-                if j == len(body_align[i])-1 or j == 0:
-                    body_temp += body_align[i][j]
-                else:
-                    body_temp += body_align[i][j] + '  '
-            
-            body_text.append(body_temp)
-        
-        space = ''
-        for item in header:
-            if item is ' ':
-                space += item
-            else:
-                break
+            self.__header += row + '\n'
 
-        dtype_temp = ''
-        stripes_temp = ''
-        header_temp = ''
-        
-        for i in range(len(header_split)):
-            if i == len(header_split)-1:
-                dtype_temp += dtype_align[i]
-                stripes_temp += stripes_align[i]
-                header_temp += header_align[i]
-            else:
-                dtype_temp += dtype_align[i]  + '  '
-                stripes_temp += stripes_align[i] + '  '
-                header_temp += header_align[i] + '  '
-        
-        items = [header_temp, dtype_temp, stripes_temp, index_name[0]]
-            
-        
-#        for i in range(len(body_text)):
-#            body_text[i] = space + body_text[i]
+        return self.__header
 
-        for i in range(len(items)):
-            items[i] = items[i]
-
-        text_header = ''
-        for i in range(len(items)):
-            text_header += items[i] + '\n'
-            
-        text_body = ''
-        for i in range(len(body_text)):
-            text_body += body_text[i] + '\n'
-            
-        total_text = text_header + text_body
-        
-        return total_text
-        
     def make_stripe(self, n):
         stripe = ''
-        
+
         for i in range(n):
             stripe += '-'
-            
+
         return stripe
-        
-df = SubclassedDataFrame({'Index': [1, 2, 3], 'CCC': [1, 2, 3], 'BBB': [4, 5, 6], 'AAA': [7, 8, 9]})
+
+
+df = SubclassedDataFrame({'DDD': [1, 2, 3], 'CCCCCCC': [10, 200, 3], 'BBB': [40000, 50, 656565645], 'AAA': [70, 0.8585858, 900]})
 self = df
 
-print df
-df2 = df.set_index('Index')
-self = df2
+# print df
+# df2 = df.set_index('Index')
+# self = df2
 
-#df = pd.DataFrame({'Index': [1, 2, 3], 'CCC': [1, 2, 3], 'BBB': [4, 5, 6], 'AAA': [7, 8, 9]})
-#df2 = df.set_index('Index')
+# df = pd.DataFrame({'Index': [1, 2, 3], 'CCC': [1, 2, 3], 'BBB': [4, 5, 6], 'AAA': [7, 8, 9]})
+# df2 = df.set_index('Index')
